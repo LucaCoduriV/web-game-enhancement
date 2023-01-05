bd7fc8c02c4001ee8902ef542cba94d6a5954127
Luca Coduri <coduri.vitor@gmail.com>
2023-01-05 01:40:22 +0100

    audio is better now

--- src/remote.js
+++ src/remote.js
@@ -3,17 +3,20 @@ import { wiimote } from "./keyboard.js";
 let playerId = 0;
 
 document.getElementById("player-id").addEventListener("change", onPlayerIdChange, true);
+document.getElementById("start").addEventListener("click", start);
 
 function onPlayerIdChange(event) {
     console.log(document.getElementById("player-id").value);
     playerId = document.getElementById("player-id").value;
 }
 
-// Initialize server connection
-const socket = new WebSocket("wss://luca-aspire-a515-54G:3001/");
+function start() {
+    // Initialize server connection
+    const socket = new WebSocket("wss://luca-aspire-a515-54G:3001/");
 
-socket.onopen = () => {
-    const listener = (event) => socket.send(JSON.stringify(event));
-    console.log("coucou");
-    wiimote(listener);
-};
+    socket.onopen = () => {
+        const listener = (event) => socket.send(JSON.stringify(event));
+        console.log("coucou");
+        wiimote(listener);
+    };
+}
